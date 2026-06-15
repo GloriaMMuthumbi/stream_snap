91b03f2beffd14ff6abdba9417cecd22f5248c9b
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2026-06-15 22:03:53 +0300

    Add video model and controller cache manager

---
+++ lib/features/video_feed/data/models/videos_post.dart
@@ -0,0 +1,13 @@
+class VideoPost {
+  final String id;
+  final String url;
+  final String username;
+  final String caption;
+
+  VideoPost(
+      {required this.id,
+      required this.url,
+      required this.username,
+      required this.caption}
+  );
+}
